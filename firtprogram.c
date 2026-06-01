1fde7b247ffde611c6c33b603c730e89a4f4ef17
Siddharth Verma <25110cn219@glbitm.ac.in>
2026-06-01 21:56:13 +0530

    Add files via upload

---
+++ firtprogram.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int n,sum=0,c,value;
+    printf("how many number you wants to add?\n");
+    scanf("%d", &n);
+    printf("enter %d integrs\n",n);
+    for(c=1;c<=n;c++)
+    return 0;
+}
