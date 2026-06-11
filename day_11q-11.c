f7101609abe24da36b2e602cd9ad1bd912c97753
Siddharth Verma <25110cn219@glbitm.ac.in>
2026-06-11 14:45:31 +0530

    Add files via upload

---
+++ day_11q-11.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+// Function to find GCD using the Euclidean algorithm
+int findGCD(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+int main() {
+    int num1, num2, gcd;
+
+    printf("Enter two integers: ");
+    if (scanf("%d %d", &num1, &num2) != 2) {
+        printf("Invalid input.\n");
+        return 1;
+    }
+
+    // Handle negative inputs by converting them to positive
+    int absoluteNum1 = (num1 < 0) ? -num1 : num1;
+    int absoluteNum2 = (num2 < 0) ? -num2 : num2;
+
+    gcd = findGCD(absoluteNum1, absoluteNum2);
+
+    printf("The GCD of %d and %d is: %d\n", num1, num2, gcd);
+
+    return 0;
+}
